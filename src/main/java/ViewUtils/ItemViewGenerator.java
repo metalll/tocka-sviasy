7e2301d628980de5df2f97399ccd00c97db837f3
lexa <linuxoid6666@gmail.com>
2016-06-03 21:46:27 +0300

    wqaegtdhjrtgrfds

--- src/main/java/ViewUtils/ItemViewGenerator.java
+++ src/main/java/ViewUtils/ItemViewGenerator.java
@@ -9,10 +9,10 @@ public class ItemViewGenerator {
 				" <div class=\"col s4\"><div class=\"row\"><div class=\"col s12\"><div class=\"card\">"+
                 " <div class=\"card-image\"><img src=\""+ item.getLogo() + "\">"+
                 " </div> <div class=\"card-content valign-wrapper\">"+
-                " <span class=\"card-title blue-text  text-darken-2\">"+item.getName()+"</span>"+
+                " <span class=\"card-title blue-text left-align  text-darken-2\">"+item.getName()+"</span>"+
                 " <h5 class=\"right-align green-text\">"+item.getPrice()+"</h5>"+
                     "</div>"+
-                    "<div class=\"card-action\">"+
+                    "<div class=\"card-action valign-wrapper\">"+
 
                         "<a class = \"green-text center-align\" >"+item.getExist()+"</a>"+
                         "<p/>"+
